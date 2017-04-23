fd79421f21d4649d43fa955058f8b91f31ea207a
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-04-23 10:06:38 -0700

    [prog] Pass first test with hacky config solution

--- server/utils/currency.js
+++ server/utils/currency.js
@@ -1,9 +1,8 @@
 const rp = require('request-promise')
+require('dotenv').config()
 
 const CURRENCY_API = `http://hey-guy.softledger.com?key=${process.env.CURRENCY_API_KEY}`
 
 module.exports = {
-  getCurrenciesFromAPI: () => {
-    return rp.get()
-  }
+  getCurrenciesFromAPI: () => rp.get(CURRENCY_API).then(currencies => JSON.parse(currencies))
 }
--- test/currency.get.spec.js
+++ test/currency.get.spec.js
@@ -1,11 +1,12 @@
 const expect = require('chai').expect
 const currencyCtrl = require('../server/utils/currency')
+require('dotenv').config()
 
 describe('Currency helpers', () => {
   it('should get all the currencies', () => {
     return currencyCtrl.getCurrenciesFromAPI()
       .then(currencies => {
-        expect(currencies).to.be.a('float32array')
+        expect(currencies).to.be.an('object')
       })
   })
 })
