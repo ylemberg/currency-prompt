7199098dec3f4b7ed589f0339fbabeb092127969
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-04-23 09:36:45 -0700

    [prog] Start on test for currency getter

--- server/controllers/currency.js
+++ server/controllers/currency.js
@@ -1,5 +1,12 @@
+const currencyUtil = require('../utils/currency')
+
 module.exports = {
   get: (req, res) => {
-    res.send('hai get')
+    currencyUtil.getCurrenciesFromAPI()
+      .then(currencies => {
+        res.send('hai get')
+      }).catch(err => {
+        res.status(500).send(err)
+      })
   }
 }
--- test/currency.get.spec.js
+++ test/currency.get.spec.js
@@ -1,7 +1,11 @@
 const expect = require('chai').expect
+const currencyCtrl = require('../server/utils/currency')
 
-describe('Currency controller', () => {
+describe('Currency helpers', () => {
   it('should get all the currencies', () => {
-
+    return currencyCtrl.getCurrenciesFromAPI()
+      .then(currencies => {
+        expect(currencies).to.be.a('float32array')
+      })
   })
 })
