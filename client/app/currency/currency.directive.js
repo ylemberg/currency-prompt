6c559af1899a2066e90a972b3cb4186997aa338c
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-04-23 15:01:04 -0700

    [fix] Try to fix 'mocha not found' CI error by chaning node version

---
+++ client/app/currency/currency.directive.js
@@ -0,0 +1,12 @@
+angular.module('currency-prompt')
+.controller('currencyCtrl', ['$scope', function ($scope) {
+  var vm = this
+}])
+.directive('currency', function () {
+  return {
+    controller: 'currencyCtrl as ctrl',
+    scope: {
+    },
+    templateUrl: 'app/currency/currency.html'
+  }
+})
