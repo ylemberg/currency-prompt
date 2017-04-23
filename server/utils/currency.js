a58bd0e35a1a96f183987747505983e3a09108e2
Yuriy Lemberg <yuriy.v.lemberg@gmail.com>
2017-04-23 09:53:56 -0700

    [setup] Environment variables for API request

--- server/utils/currency.js
+++ server/utils/currency.js
@@ -1,5 +1,9 @@
+const rp = require('request-promise')
+
+const CURRENCY_API = `http://hey-guy.softledger.com?key=${process.env.CURRENCY_API_KEY}`
+
 module.exports = {
   getCurrenciesFromAPI: () => {
-
+    return rp.get()
   }
 }
